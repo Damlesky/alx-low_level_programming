5b5cfc83a4f1c777b61de3ebe4fa27ad4922507e
damlesky <adetutudamilola94@gmail.com>
2023-02-17 16:26:14 -0800

    ommit some letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+/* using the putchar function */
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase without q and e,
+ *	followed by a new line.
+ *
+ * Return: always 0.
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != q)
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
