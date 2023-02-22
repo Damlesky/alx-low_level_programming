47a1a70b7c911d3895bd88fa64ad02c895f92d0a
damlesky <adetutudamilola94@gmail.com>
2023-02-21 16:38:13 -0800

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+/* writing a program that prints a string */
+
+#include <stdio.h>
+
+/**
+ * main - to print the word _putchar,
+ *	followed by a new line.
+ *
+ * Return: always 0.
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
