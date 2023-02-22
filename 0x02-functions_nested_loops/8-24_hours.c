f7c93ad480bc340f9f0a6440aee4dee55418c8d6
damlesky <adetutudamilola94@gmail.com>
2023-02-22 05:40:20 -0800

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@ void jack_bauer(void)
 	int hour;
 	int minutes;
 
-	for (hour = 0; hours <= 23; hour++)
+	for (hour = 0; hour <= 23; hour++)
 	{
 		for (minutes = 0; minutes <= 59; minutes++)
 		{
