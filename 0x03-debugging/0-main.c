e116f28d55da4e458cb9b7cbbc274980a8b18a2b
damlesky <adetutudamilola94@gmail.com>
2023-02-23 05:00:55 -0800

    update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,10 +5,8 @@
 * Return: 0
 */
 
-int main(void)
+void positive_or_negative(int i)
 {
-        int i;
-
 	if (i > 0)
 		printf("%d is positive", i);
 	else if (i < 0)
@@ -16,5 +14,5 @@ int main(void)
 	else
 		printf("%d is zero", i);
 
-        return (0);
+	return (0);
 }
