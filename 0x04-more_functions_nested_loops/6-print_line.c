a23fa9e541894f279a380e7e39335d28bab3b24b
damlesky <adetutudamilola94@gmail.com>
2023-02-23 17:54:31 -0800

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,11 +8,12 @@
 
 void print_line(int n)
 {
-	for (n > 0; n--)
+	int count
+
+	if (n > 0)
 	{
-		if (n <= 0)
-			_putchar('\n');
-		else
+		for (count = 0; count < n; count++)
 			_putchar('_');
 	}
+	_putchar('\n');
 }
