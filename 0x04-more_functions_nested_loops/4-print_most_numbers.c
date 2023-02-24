587072f5e72009bb13010affdd2372543afbf788
damlesky <adetutudamilola94@gmail.com>
2023-02-23 16:56:00 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers.
+*/
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n != 2 && n != 4)
+		_putchar((n % 10) + '0');
+	}
+	_putchar('\n');
+}
