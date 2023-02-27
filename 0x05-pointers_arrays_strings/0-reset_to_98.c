cbbac2c73feeb974b8c0afc366ca6ce992f941b7
damlesky <adetutudamilola94@gmail.com>
2023-02-27 13:20:24 -0800

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,14 +2,11 @@
 
 /**
  * reset_to_98 - takes a pointer to an int as parameter
- * 	and updates the value it points to to 98.
- * @*c: pointer to store variable address.
+ *	and updates the value it points to to 98.
+ * @n: pointer to store variable address.
 */
 
 void reset_to_98(int *n)
 {
-	int i;
-
-	n = &i;
 	*n = 98;
 }
