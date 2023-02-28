dc0ff6f1265efd07b7e0b20756e281b37337e015
damlesky <adetutudamilola94@gmail.com>
2023-02-27 16:22:06 -0800

    length of string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,16 @@
 
 /**
  * _strlen - returns the length of a string
+ * @s: to contain string whose length is to be determined
+ *
+ * Return: always 0.
 */
 
 int _strlen(char *s)
 {
 	int i;
 
-	for (i = 0; *s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i > 9)
 			_putchar((i / 10) + '0');
